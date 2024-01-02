029c1f0ce299b5fc6a82a536ef7af6057cd2219f
host-codes <110379028+host-codes@users.noreply.github.com>
2024-01-02 06:19:12 +0530

    Update custom_bar_due.js

--- library/custom_bar_due.js
+++ library/custom_bar_due.js
@@ -1,5 +1,5 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "https://script.google.com/macros/s/AKfycbxFWLAjEfbIEm8uMP1s5QKxx4VkaBa96V1aoE-8DmRkB5BnR54Q50nBv04l0YCUgsOP/exec";
+var url = "https://script.google.com/macros/s/AKfycbwOH0tfJ7Xy8ygyJhv5JbV3hyHEwhlwEAvQQwbK7qWXUfXtM6lYoRUF6P7MVsqsrwXP/exec";
 
 xmlhttp.open("GET",url,true);
 xmlhttp.send();
